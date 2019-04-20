bc18a22d96cdfce5a161eb0209513825886ca8f2
Surafel Nigussie Asfaw <surafel.nigussie12@gmail.com>
2019-04-20 13:57:55 -0500

    minor changes

--- my-angular-app/src/app/app.module.ts
+++ my-angular-app/src/app/app.module.ts
@@ -9,12 +9,14 @@ import { ProductComponent } from './product/product.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './home/home.component';
 import { UserComponent } from './user/user.component';
+import { EmployeeComponent } from './employee/employee.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: UserComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'product', component: ProductComponent }
+  { path: 'product', component: ProductComponent },
+  { path: 'employee', component: EmployeeComponent }
 ];
 
 @NgModule({
@@ -22,7 +24,8 @@ const appRoutes: Routes = [
     AppComponent,
     ProductComponent,
     HomeComponent,
-    UserComponent
+    UserComponent,
+    EmployeeComponent
   ],
   imports: [
     BrowserModule,
