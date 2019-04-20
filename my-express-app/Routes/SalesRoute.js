9fdd7148cb4013118221277589a70e654f684b3f
tomas <habteslasietomas@gmail.com>
2019-04-20 08:05:24 -0500

    updated the sales route

--- my-express-app/Routes/SalesRoute.js
+++ my-express-app/Routes/SalesRoute.js
@@ -1,7 +1,10 @@
 const router = require('express').Router()
 const ProductService = require('../Service/salesproduct');
 const productService = new ProductService();
+const ProductSale = require('../Service/sales');
+const productSale = new ProductSale();
 
+//usrls for geting and updating products
 router.post('/', function (req, res, next) {
     productService.add(req.body)
       .then(() => res.status(200).json({
@@ -19,7 +22,7 @@ router.post('/', function (req, res, next) {
       );
     
     });
-    router.get('/:type', function (req, res, next) {
+    router.get('/catagory/:type', function (req, res, next) {
         const queryObj = {"type":req.params.type};
         productService.getAll(queryObj).subscribe(
             (users) => res.status(200).json(users),
@@ -28,4 +31,32 @@ router.post('/', function (req, res, next) {
           );
         
         });
+        router.get('/name/:name', function (req, res, next) {
+            const queryObj = {"name":req.params.name};
+            productService.getAll(queryObj).subscribe(
+                (users) => res.status(200).json(users),
+                (err) => next(err),
+                null
+              );
+            
+            });
+
+
+            //usrls for sale
+            router.get('/sales', function (req, res, next) {
+                const queryObj = {};
+                productSale.getAll(queryObj).subscribe(
+                (users) => res.status(200).json(users),
+                (err) => next(err),
+                null
+                );
+    
+                });
+                router.post('/sales', function (req, res, next) {
+                    productService.add(req.body)
+                      .then(() => res.status(200).json({
+                        success: true
+                      }))
+                      .catch((err) => next(err));
+                  });
 module.exports = router
