074a7e6abcaf50eb5fa114aea492b8aed6a403a7
tomas <tomhabtt@gmail.com>
2019-04-20 21:27:42 -0500

    updated category

--- my-angular-app/src/app/salesform/category.service.ts
+++ my-angular-app/src/app/salesform/category.service.ts
@@ -11,27 +11,7 @@ export class CategoryService {
 
   }
 
-  verifyemail(emailModel: Object) {
-    return this.http.post(this.baseUrl + '/auth/verifyemail', emailModel)
-  }
-
-  register(userModel: UserModel): Observable<UserResponse> {
-    return this.http.post<UserResponse>(this.baseUrl + '/auth/register', userModel)
-      .pipe(
-        tap((user: UserResponse) => console.log(user))
-      );
-  }
-
-  login(userModel: UserModel): Observable<UserResponse> {
-    return this.http.post<UserResponse>(this.baseUrl + '/auth/login', userModel)
-      .pipe(
-        tap((user: UserResponse) => console.log(user))
-      );
-  }
-
-  getSupplierByName(name: String): Observable<SupplierResponse> {
-    return this.http.get<SupplierResponse>(this.baseUrl + '/products/' + name)
-  }
+  
 }
 
 export interface ErrorModel {
@@ -39,32 +19,6 @@ export interface ErrorModel {
   message: string
 }
 
-export interface UserModel {
-  firstname: string,
-  lastname: string,
-  email: string,
-  password: string
-}
 
-export interface UserResponse {
-  auth: boolean,
-  token: string,
-  hasError: boolean,
-  message: string
-}
-
-export interface SupplierModel {
-  name: String,
-  address: String,
-  phone: String,
-  email: String,
-  type: String
-}
-
-export interface SupplierResponse {
-  suppliers: [SupplierModel],
-  hasError: false,
-  message: "Success"
-}
 
 
